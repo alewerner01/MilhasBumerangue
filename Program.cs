ad4269d6335d0b8559d9b039435f3aebe5bcea65
alewerner01 <162760202+alewerner01@users.noreply.github.com>
2024-04-05 22:08:40 -0300

    Projeto

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿double percentualDeBonus, percentualDeRetorno;
+double pontosATransferir, pontosAReduzir, pontosAAcrescentar;
+
+Console.Clear();
+Console.WriteLine("--- Milhas Bumerangue ---\n");
+
+Console.WriteLine("percentual De bonus.....:");
+percentualDeBonus = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Percentual De Retorno...:");
+percentualDeRetorno = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Pontos A Transferir.....:");
+pontosATransferir = Convert.ToDouble(Console.ReadLine());
+
+pontosAReduzir = pontosATransferir - (pontosATransferir * percentualDeRetorno) / 100;
+pontosAAcrescentar = pontosATransferir + (pontosATransferir * percentualDeBonus) / 100;
+
+Console.WriteLine();
+Console.WriteLine($"Pontos a reduzir na origem........: {pontosAReduzir}");
+Console.WriteLine($"Pontos A Acrescentar no destino...: {pontosAAcrescentar}");
+
